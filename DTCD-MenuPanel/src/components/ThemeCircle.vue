113aabff48fd49cfad832d534b22a1908955a8e2
Konstantin <konstantin@isgneuro.com>
2021-03-12 19:10:45 +0300

    Tests inited

---
+++ DTCD-MenuPanel/src/components/ThemeCircle.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="theme-btn"></button>
+</template>
+
+<script>
+export default {
+  props: {
+    selected,
+  },
+};
+</script>
+
+<style scoped>
+.theme-btn {
+  height: 2rem;
+  width: 2rem;
+  border: none;
+  border-radius: 50%;
+  margin: 3px;
+}
+
+.theme-btn:focus {
+  border: none;
+  outline: none;
+}
+.selected {
+  border: 3px solid rgb(0, 0, 0);
+}
+</style>
