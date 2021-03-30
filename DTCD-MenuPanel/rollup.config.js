382787c75169dbefb1e5afe19d6e6609e4c0090a
sbelikov98 <sbelikov@isgneuro.com>
2021-03-30 11:48:15 +0300

    Updated rollup.config.js file

--- DTCD-MenuPanel/rollup.config.js
+++ DTCD-MenuPanel/rollup.config.js
@@ -1,44 +1,43 @@
-import path from 'path';
-
+import vue from 'rollup-plugin-vue2';
 import alias from '@rollup/plugin-alias';
 import styles from 'rollup-plugin-styles';
-import vue from 'rollup-plugin-vue2';
 import replace from '@rollup/plugin-replace';
+
+import path from 'path';
+import pluginMeta from './src/Plugin.Meta';
 
 const watch = Boolean(process.env.ROLLUP_WATCH);
 
-const pluginName = 'MenuPanel';
-
-const projectSrcDir = path.join(path.resolve(__dirname), 'src');
-
-const fileDest = watch ? `./../../DTCD/server/plugins/DTCD-${pluginName}/${pluginName}.js` : `./build/${pluginName}.js`;
+const pluginName = pluginMeta.name;
+const outputFile = `${pluginName}.js`;
+const outputDirectory = watch ? `./../../DTCD/server/plugins/DTCD-${pluginName}` : `./build`;
 
 const plugins = [
-	alias({
-		entries: {
-			'@': projectSrcDir,
-		},
-	}),
-	vue(),
-	styles({
-		mode: 'inject',
-	}),
-	replace({
-		preventAssignment: true,
-		'process.env.NODE_ENV': JSON.stringify('production'),
-		'process.env.VUE_ENV': JSON.stringify('browser'),
-	}),
+  vue(),
+  alias({
+    entries: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  }),
+  styles({
+    mode: 'inject',
+  }),
+  replace({
+    preventAssignment: true,
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env.VUE_ENV': JSON.stringify('browser'),
+  }),
 ];
 
 export default {
-	input: `src/Plugin.js`,
-	output: {
-		file: fileDest,
-		format: 'esm',
-		sourcemap: false,
-	},
-	watch: {
-		include: ['./src/**'],
-	},
-	plugins,
+  plugins,
+  input: `src/Plugin.js`,
+  output: {
+    file: `${outputDirectory}/${outputFile}`,
+    format: 'esm',
+    sourcemap: false,
+  },
+  watch: {
+    include: ['./src/**'],
+  },
 };
