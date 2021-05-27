ac53bbd7d4a9acea29ebea8e96ccd2819046e0eb
romankuramshin <rkuramshin@isgneuro.com>
2021-05-27 13:36:24 +0300

    added back button

--- DTCD-MenuPanel/src/components/MenuPanel.vue
+++ DTCD-MenuPanel/src/components/MenuPanel.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="container">
     <div class="button-container">
-      <div
-        id="addNewPanelBtn"
-        class="btn"
-        title="Добавить панель"
-        @click="addNewWorkspacePanel"
-      >
+      <div id="addNewPanelBtn" class="btn" title="Добавить панель" @click="addNewWorkspacePanel">
         <div class="icon">
-          <i class="fas fa-plus"/>
+          <i class="fas fa-plus" />
+        </div>
+      </div>
+      <div
+        id="changeModeBtn"
+        class="btn edit-mode-btn"
+        :class="{ active: isEditModeEnabled }"
+        :title="editModeButtonTitle"
+        @click="changeEditMode"
+      >
+        <div v-if="isEditModeEnabled" key="view-mode" class="icon">
+          <i class="far fa-eye" />
+        </div>
+        <div v-else key="edit-mode" class="icon">
+          <i class="fas fa-pencil-alt" />
         </div>
       </div>
       <transition name="fade" mode="out-in">
@@ -20,34 +29,43 @@
           @click="compactWorkspacePanels"
         >
           <div class="icon">
-            <i class="fas fa-outdent"/>
+            <i class="fas fa-outdent" />
           </div>
         </div>
       </transition>
     </div>
     <div class="button-container">
       <div
-        id="changeModeBtn"
-        class="btn edit-mode-btn"
-        :class="{ active: isEditModeEnabled }"
-        :title="editModeButtonTitle"
-        @click="changeEditMode"
+        id="workspaceSelection"
+        class="btn"
+        title="Вернуться к выбору рабочего стола"
+        @click="backToWorkspaces"
       >
-        <div v-if="isEditModeEnabled" key="view-mode" class="icon">
-          <i class="far fa-eye"/>
-        </div>
-        <div v-else key="edit-mode" class="icon">
-          <i class="fas fa-pencil-alt"/>
+        <div class="icon">
+          <i class="fas fa-arrow-circle-left"></i>
         </div>
       </div>
     </div>
+    <!-- <div v-if="isEditModeEnabled" class="menu-panel-theme-container">
+      <p>Выбрать тему:</p>
+      <div class="themes-container">
+        <div
+          class="theme-btn"
+          :class="{ selected: theme.name === selectedTheme }"
+          v-for="theme in themeList"
+          :key="theme.name"
+          :style="{ background: theme.preview }"
+          @click="changeTheme(theme.name)"
+        ></div>
+      </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'MenuPanel',
-  data: (self) => ({
+  data: self => ({
     guid: self.$root.guid,
     eventSystem: self.$root.eventSystem,
     styleSystem: self.$root.styleSystem,
@@ -55,44 +73,51 @@ export default {
     isEditModeEnabled: false,
   }),
   computed: {
-    editModeButtonTitle () {
+    editModeButtonTitle() {
       const mode = this.isEditModeEnabled ? 'просмотра' : 'редактирования';
       return `Включить режим ${mode}`;
-    }
+    },
   },
-  async mounted () {
-    this.eventSystem.createActionByCallback('updateTheme', this.guid, this.updateTheme.bind(this));
-    this.eventSystem.subscribeByNames('ThemeUpdate', 'updateTheme');
-    const themes = await this.styleSystem.getThemes();
-    this.themeList.push(...themes);
+  async mounted() {
+    // this.eventSystem.createActionByCallback('updateTheme', this.guid, this.updateTheme.bind(this));
+    // this.eventSystem.subscribeByNames('ThemeUpdate', 'updateTheme');
+    // const themes = this.styleSystem.getThemes();
+    // this.themeList.push(...themes);
   },
   methods: {
-    addNewWorkspacePanel () {
-      this.eventSystem.createAndPublish(this.guid, 'DefaultAddWorkspacePanel');
+    addNewWorkspacePanel() {
+      PubSub.publish('DefaultAddWorkspacePanel[' + this.guid + ']');
     },
 
-    compactWorkspacePanels () {
-      this.eventSystem.createAndPublish(this.guid, 'CompactWorkspacePanel');
+    compactWorkspacePanels() {
+      PubSub.publish('CompactWorkspacePanel[' + this.guid + ']');
     },
 
-    changeEditMode () {
+    changeEditMode() {
       this.isEditModeEnabled = !this.isEditModeEnabled;
-      this.eventSystem.createAndPublish(this.guid, 'ChangeWorkspaceEditMode');
+      PubSub.publish('ChangeWorkspaceEditMode[' + this.guid + ']');
     },
-
-    updateTheme () {
-      const theme = this.styleSystem.getCurrentTheme();
-      this.styleSystem.setVariablesToElement(this.$el, theme);
+    backToWorkspaces() {
+      PubSub.publish('BackToWorkspaceSelection[' + this.guid + ']');
     },
+    // changeTheme(name) {
+    //   this.selectedTheme = name;
+    //   this.styleSystem.setTheme(this.selectedTheme);
+    //   this.eventSystem.createAndPublish(this.guid, 'ThemeUpdate');
+    // },
+    // updateTheme() {
+    //   const theme = this.styleSystem.getCurrentTheme();
+    //   this.styleSystem.setVariablesToElement(this.$el, theme);
+    // },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @import './../styles/base';
 
-$c-blue: #2196F3;
-$c-green: #4CAF50;
+$c-blue: #2196f3;
+$c-green: #4caf50;
 
 .container {
   display: flex;
@@ -112,7 +137,7 @@ $c-green: #4CAF50;
     align-items: center;
     width: 40px;
     height: 40px;
-    border: thin solid rgba(0, 0, 0, .25);
+    border: thin solid rgba(0, 0, 0, 0.25);
     border-radius: 3px;
     cursor: pointer;
     margin-left: 20px;
@@ -127,8 +152,8 @@ $c-green: #4CAF50;
       border-color: $c-blue;
 
       & .icon {
-          color: $c-blue;
-        }
+        color: $c-blue;
+      }
     }
 
     &.edit-mode-btn {
@@ -155,6 +180,24 @@ $c-green: #4CAF50;
       font-size: 16px;
       transition: $transition-time;
     }
+  }
+  .menu-panel-theme-container {
+    padding: 5px;
+  }
+  .themes-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .theme-btn {
+    height: 2rem;
+    width: 2rem;
+    border-radius: 50%;
+    margin: 3px;
+    border: 3px solid transparent;
+    transition: 0.3s;
+  }
+  .selected {
+    border-color: black;
   }
 }
 </style>
